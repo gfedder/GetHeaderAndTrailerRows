69bb851463ccde1e4d44d2330c0ba6fe60c2cc23
gFedz <38161940+gfedder@users.noreply.github.com>
2023-02-15 10:15:18 -0600

    Upload of script

---
+++ GetHeaderAndTrailerRows.ps1
@@ -0,0 +1,9 @@
+﻿$fullPathOfFile = ""
+
+Clear
+
+Write-Host "Header (First Row): "
+Get-Content $fullPathOfFile | Select -First 1
+
+Write-Host "`nTrailer (Last Row): "
+Get-Content $fullPathOfFile -Tail 2
